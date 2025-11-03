7075242c5aa2c7d2490216eb96cc14f140cd4297
Ebraam Sobhy <ibraam.sobhy2003@gmail.com>
2025-11-03 22:46:38 +0200

    first commit

--- __tests__/math.test.js
+++ __tests__/math.test.js
@@ -1,9 +1,58 @@
-const { add, subtract } = require('../src/math');
+const { add, subtract, multiply, myFunction, fetchData, fetchPromise } =  require('../src/math.js');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(add(1, 2)).toBe(3);
+test("testing the math opertators add", () => {
+    expect(add(2,3)).toBe(5)
 });
 
-test('subtracts 5 - 3 to equal 2', () => {
-  expect(subtract(5, 3)).toBe(2);
+test("testing the math opertators subtract", () => {
+    expect(subtract(5,3)).toBe(2)
 });
+
+test("testing the math opertators multiply", () => {
+    expect(multiply(2,4)).toBe(8)
+});
+
+// Describe and Matchers
+
+describe('Matchers', () => {
+
+    test('Multiply', () => {
+        expect(5 * 10).toBe(50);
+    })
+
+    test('Sum', () => {
+        expect(5 + 10).toBe(15);
+    })
+
+})
+
+test('Null is False', () => {
+    const n = null;
+    expect(n).toBeFalsy();
+})
+
+// Function Throw error
+test('throw on invalid imput', () => {
+    expect(() => {
+        myFunction(invalid);
+    }).toThrow();
+})
+
+// Async/Await Testing
+test('testing data', done => {
+    function callback(data) {
+        try {
+            expect(data).toBe('coding');
+            done();
+        } catch(error) {
+            done(error);
+        }
+    }
+
+    fetchData(callback);
+})
+
+// Promise
+test('Promise data', () => {
+    return expect(fetchPromise()).resolves.toBe('coding');
+})
--- src/math.js
+++ src/math.js
@@ -1,9 +1,43 @@
+// Add two numbers
 function add(a, b) {
   return a + b;
 }
 
+// Subtract two numbers
 function subtract(a, b) {
   return a - b;
 }
 
-module.exports = { add, subtract };
+// Multiply two numbers
+function multiply(a, b) {
+  return a * b;
+}
+
+// Divide two numbers safely
+function divide(a, b) {
+  if (b === 0) throw new Error("Cannot divide by zero");
+  return a / b;
+}
+
+function myFunction(input) {
+    if (typeof input !== 'number') {
+        throw new Error('Invalid input')
+    }
+}
+
+function fetchData(callback) {
+    setTimeout(()=> {
+        callback('coding');
+    }, 1000)
+}
+
+function fetchPromise() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => resolve('coding'), 1000);
+    });
+}
+
+// Export functions
+module.exports = { add, subtract, multiply, divide, myFunction, fetchData, fetchPromise };
+
+
